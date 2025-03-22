d7468ca9c13b1c81c0745e74f785568a74565a3e
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-22 00:36:33 -0700

    Methods to combine 2 strings in JSX

--- 281-javascript-expressions-in-jsx/src/index.jsx
+++ 281-javascript-expressions-in-jsx/src/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Manvi";
+const fname = "Manvi";
+const lname = "Sharma";
 const num = 6;
 
 ReactDOM.render(
     <div>
-        <h1>Hello {name}!</h1>
+        {/* Method 1 to combine 2 strings in JSX  */}
+        <h1>Hello {fname + " " + lname}!</h1>
+         {/* Method 2 to combine 2 strings in JSX  */}
+        {/* <h1>Hello {fname} {lname}!</h1> */}
         <p>Your lucky number is {Math.floor(Math.random() * 10)}.</p>
-    </div>, document.getElementById("root"));
+    </div>, 
+    document.getElementById("root")
+);
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
