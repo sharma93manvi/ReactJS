64d1be2f7901b28494fcca71708209d633121257
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-22 00:38:48 -0700

    Method 3: JS inserted as string in JSX using ES6

--- 281-javascript-expressions-in-jsx/src/index.jsx
+++ 281-javascript-expressions-in-jsx/src/index.jsx
@@ -8,9 +8,13 @@ const num = 6;
 ReactDOM.render(
     <div>
         {/* Method 1 to combine 2 strings in JSX  */}
-        <h1>Hello {fname + " " + lname}!</h1>
-         {/* Method 2 to combine 2 strings in JSX  */}
+        {/* <h1>Hello {fname + " " + lname}!</h1> */}
+
+        {/* Method 2 to combine 2 strings in JSX  */}
         {/* <h1>Hello {fname} {lname}!</h1> */}
+
+        {/* Method 3 to combine 2 strings in JSX using ES6 */}
+        <h1>Hello {`${fname}  ${lname}`}!</h1>
         <p>Your lucky number is {Math.floor(Math.random() * 10)}.</p>
     </div>, 
     document.getElementById("root")
