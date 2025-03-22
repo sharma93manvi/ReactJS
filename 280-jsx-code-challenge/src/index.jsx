32a952524db52dcffb700f1cf01fd3fa4ce9370f
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-21 23:50:33 -0700

    Create React app from the scratch & add some content

--- 280-jsx-code-challenge/src/index.jsx
+++ 280-jsx-code-challenge/src/index.jsx
@@ -1,7 +1,21 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDom from "react-dom";
+
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+ReactDom.render(
+    <div>
+        <h1>Hello! My favourite foods are:</h1>
+        <ul>
+            <li>Pizza</li>
+            <li>Pasta</li>
+            <li>Noodles</li>
+        </ul>
+    </div>, 
+    document.getElementById("root")
+);
 
 // If you're running this locally in VS Code use the commands:
 // npm install
