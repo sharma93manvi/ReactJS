5f6472cac03a0ab49050252ea8fced53250ff560
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-22 00:27:05 -0700

    Use JS expression in JSX

---
+++ 281-javascript-expressions-in-jsx/src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Manvi";
+
+ReactDOM.render(<h1>Hello {name}!</h1>, document.getElementById("root"));
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
