ea2af3c504bca61d65d3a7914e23bee6cb3105a8
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-22 00:44:08 -0700

    JS Expressions in JSX practice

---
+++ 282-javascript-expressions-in-jsx-practice-completed/src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Manvi";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
+
