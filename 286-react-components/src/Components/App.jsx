1ecbfe8ccf4b0b868478b2c654eca5658516650a
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-28 15:57:02 -0700

    287-react-components-practice

--- 286-react-components/src/Components/App.jsx
+++ 286-react-components/src/Components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Heading from "../Heading";
-import List from "../List";
+import Heading from "./Heading";
+import List from "./List";
 
 function App() {
     return (
