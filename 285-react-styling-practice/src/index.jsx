44b2aab825bfc63e1f35ff52dbafac3e5b3e501c
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-28 00:24:29 -0700

    Add heading & styling

--- 285-react-styling-practice/src/index.jsx
+++ 285-react-styling-practice/src/index.jsx
@@ -1,13 +1,16 @@
 //Create a React app from scratch.
+import React from "react";
+import ReactDom from "react-dom";
+
 //Show a single h1 that says "Good morning" if between midnight and 12PM.
 //or "Good Afternoon" if between 12PM and 6PM.
 //or "Good evening" if between 6PM and midnight.
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+ReactDom.render (
+        <h1 className="heading">Good morning</h1>,
+        document.getElementById("root")
+);
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
