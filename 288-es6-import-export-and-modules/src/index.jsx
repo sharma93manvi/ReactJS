851d7d02245bb0da59e5bf07b3cd43589a4f5852
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-28 21:23:16 -0700

    export & import using * as pi

--- 288-es6-import-export-and-modules/src/index.jsx
+++ 288-es6-import-export-and-modules/src/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi, { doublePi, triplePi } from "./Math";
+// import pi, { doublePi, triplePi } from "./Math";
+import * as pi from "./Math";
 
 ReactDOM.render(
   <ul>
-    <li>{pi}</li>
-    <li>{doublePi()}</li>
-    <li>{triplePi()}</li>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
+    {/* <li>{doublePi()}</li>
+    <li>{triplePi()}</li> */}
   </ul>,
   document.getElementById("root")
 );
