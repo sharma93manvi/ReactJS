02d3f53901455a92ac01c060042ffba7807ff012
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-28 21:18:20 -0700

    export & import default pi

--- 288-es6-import-export-and-modules/src/index.jsx
+++ 288-es6-import-export-and-modules/src/index.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import pi from "./Math";
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
+    <li>{pi}</li>
     <li>2</li>
     <li>3</li>
   </ul>,
   document.getElementById("root")
 );
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
