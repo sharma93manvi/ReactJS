6ed235ad1e88fa513f18075e028ec560b1797854
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-29 22:51:55 -0700

    Remove repetative content & create function Card

--- 295-react-props/src/index.jsx
+++ 295-react-props/src/index.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-
+function Card() {
+  return <div>
     <h2>Beyonce</h2>
     <img
       src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
@@ -12,28 +10,14 @@ ReactDOM.render(
     />
     <p>+123 456 789</p>
     <p>b@beyonce.com</p>
+  </div>
+}
 
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
-    />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
-
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
-    />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
   </div>,
   document.getElementById("root")
 );
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
