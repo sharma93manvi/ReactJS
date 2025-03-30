5a1b7251c8d6754ab9d20c21a4be29df43f7d6b2
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-30 00:20:56 -0700

    Move the reusable content to Card component

--- 296-react-props-practice/src/components/App.jsx
+++ 296-react-props-practice/src/components/App.jsx
@@ -1,30 +1,12 @@
 import React from "react";
-
-function Card(props) {
-  
-  console.log(props);
-
-  return <div>
-    <div className="card">
-          <div className="top">
-            <h2 className="name">{props.name}</h2>
-            <img className="circle-img"
-              src={props.img}
-              alt="avatar_img"
-            />
-          </div>
-          <div className="bottom">
-            <p>{props.tel}</p>
-            <p>{props.email}</p>
-          </div>
-        </div>
-    </div>
-}
+import Card from "./Card";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" tel="+123 456 789" email="b@beyonce.com" />
+      <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" tel="+123 456 789" email="b@beyonce.com" />
       <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" tel="+123 456 789" email="b@beyonce.com" />
     </div>
   );
