999f41440c1df27abbe2edcb26c0d59f2e1a387f
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-30 00:17:09 -0700

    Use props to render the default Beyonce contact card

--- 296-react-props-practice/src/components/App.jsx
+++ 296-react-props-practice/src/components/App.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 
+function Card(props) {
+  
+  console.log(props);
+
+  return <div>
+    <div className="card">
+          <div className="top">
+            <h2 className="name">{props.name}</h2>
+            <img className="circle-img"
+              src={props.img}
+              alt="avatar_img"
+            />
+          </div>
+          <div className="bottom">
+            <p>{props.tel}</p>
+            <p>{props.email}</p>
+          </div>
+        </div>
+    </div>
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">Beyonce</h2>
-          <img className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" tel="+123 456 789" email="b@beyonce.com" />
     </div>
   );
 }
