22e8fa260c1100e082817fc5257c0bbd32bf93a4
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-03 00:59:24 -0700

    FindIndex - find the index of the first item that matches

--- 300-map-filter-reduce/src/index.jsx
+++ 300-map-filter-reduce/src/index.jsx
@@ -54,15 +54,13 @@ var numbers = [3, 56, 2, 48, 5];
 // console.log(newNumber);
 
 //Find - find the first item that matches from an array.
-const newNumber = numbers.find(function(num){
+// const newNumber = numbers.find(function(num){
+//     return num >10;
+// })
+// console.log(newNumber);
+
+//FindIndex - find the index of the first item that matches.
+const newNumber = numbers.findIndex(function(num){
     return num >10;
 })
 console.log(newNumber);
-
-//FindIndex - find the index of the first item that matches.
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
