e4efc464ec8e12b5e6000d4aeed2f4e4712b664d
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-03 00:49:00 -0700

    Filter using ForEach - Create a new array by keeping the items that return true

--- 300-map-filter-reduce/src/index.jsx
+++ 300-map-filter-reduce/src/index.jsx
@@ -20,11 +20,22 @@ var numbers = [3, 56, 2, 48, 5];
 // console.log(newNumbers);
 
 //Filter - Create a new array by keeping the items that return true.
-const newNumbers = numbers.filter(function(num){
-    return num<10;
-});
+// const newNumbers = numbers.filter(function(num){
+//     return num<10;
+// });
 
+// console.log(newNumbers);
+
+//-----for each - Filter------
+var newNumbers = [];
+
+numbers.forEach(function(num){
+    if(num<10){
+        newNumbers.push(num);
+    }
+});
 console.log(newNumbers);
+
 //Reduce - Accumulate a value by doing something to each item in an array.
 
 //Find - find the first item that matches from an array.
