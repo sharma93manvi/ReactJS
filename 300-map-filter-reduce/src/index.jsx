c84f3a68f97c674e0c0f09c30ab7a68bb4dec7d6
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-03 00:56:01 -0700

    Reduce - Accumulate a value by doing something to each item in an array

--- 300-map-filter-reduce/src/index.jsx
+++ 300-map-filter-reduce/src/index.jsx
@@ -37,11 +37,21 @@ var numbers = [3, 56, 2, 48, 5];
 // console.log(newNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-//-----for each - Reduce------
-var newNumber = [];
-numbers.forEach(function(currentNumber) {
-    newNumber += currentNumber;
+
+var newNumber = numbers.reduce(function(accumulator, currentNumber){
+    console.log("accumulator="+accumulator);
+    console.log("currentNumber="+currentNumber);
+    return accumulator+currentNumber;
 })
+
+console.log(newNumber);
+
+//-----for each - Reduce------
+// var newNumber = [];
+// numbers.forEach(function(currentNumber) {
+//     newNumber += currentNumber;
+// })
+// console.log(newNumber);
 //Find - find the first item that matches from an array.
 
 //FindIndex - find the index of the first item that matches.
