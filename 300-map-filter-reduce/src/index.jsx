961320f9081f0f5c3981b50d8d378eba88dedc12
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-03 00:46:56 -0700

    Filter - Create a new array by keeping the items that return true

--- 300-map-filter-reduce/src/index.jsx
+++ 300-map-filter-reduce/src/index.jsx
@@ -1,6 +1,6 @@
 var numbers = [3, 56, 2, 48, 5];
 
-//Map -Create a new array by doing something with each item in an array.
+//-----Map -Create a new array by doing something with each item in an array.
 // function double(x){
 //     return x*2;
 // }
@@ -10,18 +10,21 @@ var numbers = [3, 56, 2, 48, 5];
 
 //-----for each------
 
-var newNumbers = [];
+// var newNumbers = [];
 
-function double(x){
-    newNumbers.push(x*2);
-}
+// function double(x){
+//     newNumbers.push(x*2);
+// }
 
-numbers.forEach(double);
-console.log(newNumbers);
+// numbers.forEach(double);
+// console.log(newNumbers);
 
 //Filter - Create a new array by keeping the items that return true.
+const newNumbers = numbers.filter(function(num){
+    return num<10;
+});
 
-
+console.log(newNumbers);
 //Reduce - Accumulate a value by doing something to each item in an array.
 
 //Find - find the first item that matches from an array.
