84e37b5cb9bbc7e7d47fc862e928a774f2c2f802
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-04 16:30:18 -0700

    declarative programming focuses on what the UI should look like, while imperative programming focuses on how to achieve that look

--- 305-introduction-to-state-completed/src/components/App.jsx
+++ 305-introduction-to-state-completed/src/components/App.jsx
@@ -3,22 +3,23 @@ import React from "react";
 var isDone = false;
 
 function strike() {
-  document.getElementById("root").style.textDecoration = "line-through";
+  // document.getElementById("root").style.textDecoration = "line-through";
+  isDone = true;
 }
 
 function unStrike() {
-  document.getElementById("root").style.textDecoration = null;
+  // document.getElementById("root").style.textDecoration = null;
+  isDone = false;
 }
 
 function App() {
-  return  <p> Buy Milk </p>;
-    // <div>
-    //   {
-    //      <p style={isDone ? { textDecoration: "line-through" } : null}>Buy milk</p>
-    //     <button onClick={strike}>Change to strike through</button>
-    //     <button onClick={unStrike}>Change back</button> 
-    //   }
-    // </div>
+  return  (
+    <div>
+      {
+        <p style={isDone ? {textDecoration: "line-through"} : null }>Buy milk</p>
+      }
+    </div>
+  );
 }
 
 export default App;
