c733cbbeb00f9e22cd6a79bb57dcf165e9637165
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-04 16:47:02 -0700

    Call ReactDom.render all over again inside the function & count on screen will increase

--- 306-useState-hook/src/index.jsx
+++ 306-useState-hook/src/index.jsx
@@ -5,7 +5,14 @@ var count = 0;
 
 function increase() {
   count++;
-  console.log(count);
+  //Call ReactDom.render all over again inside the function & count on screen will increase
+  ReactDOM.render(
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+    </div>,
+    document.getElementById("root")
+  );
 }
 
 ReactDOM.render(
