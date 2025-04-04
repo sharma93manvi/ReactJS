ffd71ddb3f11d43129f466ab686d2126f56be87b
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-03 22:42:26 -0700

    Simplify the code using arrow functions

--- 301-es6-arrow-functions/src/index.jsx
+++ 301-es6-arrow-functions/src/index.jsx
@@ -4,35 +4,20 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 
-////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+//Map -Create a new array by doing something with each item in an array.
+const newNumbers1 = numbers.map(x => x * 2);
 
-//////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+////Filter - Create a new array by keeping the items that return true.
+const newNumbers2 = numbers.filter(num => num < 10);
 
-//Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+////Reduce - Accumulate a value by doing something to each item in an array.
+var newNumber1 = numbers.reduce(accumulator, currentNumber => accumulator + currentNumber);
 
-////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+//Find - find the first item that matches from an array.
+const newNumber2 = numbers.find(num => num > 10);
 
-////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+//FindIndex - find the index of the first item that matches.
+const newNumber3 = numbers.findIndex(num => num > 10);
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
