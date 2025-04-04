dfa26f8a72f5c32b21e88a231094f22bbc48b75b
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-04 00:21:26 -0700

    Use another Ternary Condition to either output a different value or null

--- 303-conditional-rendering/src/components/App.jsx
+++ 303-conditional-rendering/src/components/App.jsx
@@ -3,11 +3,15 @@ import Login from "./login";
 import Header from "./Header";
 
 var isLoggedIn = false;
+var currentTime = new Date().getHours();
+
+console.log(currentTime);
 
 function App() {
   return (
     <div className="container">{
-      isLoggedIn === true ? <Header /> : <Login />  
+      // isLoggedIn === true ? <Header /> : <Login />  
+      currentTime > 12 ? <h1> Why are you still working? </h1> : null
     } 
     </div>
   );
