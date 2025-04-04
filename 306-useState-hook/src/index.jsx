7cadedf91cbab301c2682afa0e3ab7bb85b8ea22
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-04 16:42:31 -0700

    Create variable count & add increase function

--- 306-useState-hook/src/index.jsx
+++ 306-useState-hook/src/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+var count = 0;
+
+function increase() {
+  console.log("I got clicked!");
+}
+
 ReactDOM.render(
   <div className="container">
-    <h1>0</h1>
-    <button>+</button>
+    <h1>{count}</h1>
+    <button onClick={increase}>+</button>
   </div>,
   document.getElementById("root")
 );
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
