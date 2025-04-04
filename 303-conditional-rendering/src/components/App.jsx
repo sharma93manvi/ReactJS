504aa70d52b0af14ca7f800680abeddec1dbb7c4
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-03 23:58:33 -0700

    Render Conditionally using function

--- 303-conditional-rendering/src/components/App.jsx
+++ 303-conditional-rendering/src/components/App.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
+var isLoggedIn = true;
+
+function renderConditionally(){
+  if(isLoggedIn){
+    return <h1>Hello</h1>;
+  }else{
+    return (
       <form className="form">
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <button type="submit">Login</button>
       </form>
+    )
+  }
+
+}
+
+
+function App() {
+  return (
+    <div className="container">
+      {renderConditionally()}
     </div>
   );
 }
