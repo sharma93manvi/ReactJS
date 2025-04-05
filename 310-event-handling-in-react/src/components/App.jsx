bfe172cd78ed81bd430e82910bf6175b7033c4a6
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-05 01:25:31 -0700

    handle click using function & onClick

--- 310-event-handling-in-react/src/components/App.jsx
+++ 310-event-handling-in-react/src/components/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 function App() {
+
+  function handleClick(){
+    console.log("clicked!");
+  }
+
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
