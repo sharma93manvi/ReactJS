58f38f0d0f86a5c58b2a90b6dd82bd697f464efd
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-04-04 17:16:31 -0700

    Move Index.js code to App.jsx & use Hooks to

--- 306-useState-hook/src/components/App.jsx
+++ 306-useState-hook/src/components/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return <div />;
-}
+    const state =  useState(123);
+    console.log(state[0]);
 
+    function increase() {
+      // count++;
+    }
+
+    return(
+      <div className="container">
+        <h1>{state[0]}</h1>
+        <button onClick={increase}>+</button>
+      </div>
+    );
+}
 export default App;
