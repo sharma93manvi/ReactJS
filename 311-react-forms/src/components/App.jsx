03133a89c470be40db69ad43986446650a9a42e0
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-05-07 12:13:51 -0700

    handle Submit button

--- 311-react-forms/src/components/App.jsx
+++ 311-react-forms/src/components/App.jsx
@@ -3,24 +3,32 @@ import { useState } from "react";
 
 function App() {
 
+  //Use name as single source of truth
   const [name, setName] = useState("");
+  const [header, setHeader] = useState("");
 
   function handleChange() {
     // console.log(event.target.placeholder);
     // console.log(event.target.type);
-    console.log(event.target.value);
+    // console.log(event.target.value);
     setName(event.target.value);
+  }
+
+  function handleSubmit() {
+    console.log("Submitted!");
+    setHeader(name);
   }
 
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
+      <h1>Hello {header}</h1>
       <input 
         onChange={handleChange}
         type="text" 
         placeholder="What's your name?" 
+        // value={name}
       />
-      <button>Submit</button>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
