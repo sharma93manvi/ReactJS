9fe507251fd0b2e00e7fbf97b89fb329aa507c67
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-05-07 12:16:22 -0700

    Add React forms

--- 311-react-forms/src/components/App.jsx
+++ 311-react-forms/src/components/App.jsx
@@ -22,13 +22,15 @@ function App() {
   return (
     <div className="container">
       <h1>Hello {header}</h1>
-      <input 
-        onChange={handleChange}
-        type="text" 
-        placeholder="What's your name?" 
-        // value={name}
-      />
-      <button onClick={handleSubmit}>Submit</button>
+      <form onClick={handleSubmit}>
+        <input 
+          onChange={handleChange}
+          type="text" 
+          placeholder="What's your name?" 
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
