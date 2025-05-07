c18a235cb1443be9947114803f06dcb86e022bca
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-05-06 21:37:06 -0700

    Update files

--- 310-event-handling-in-react/src/components/App.jsx
+++ 310-event-handling-in-react/src/components/App.jsx
@@ -1,16 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
-
+  const [headingText, setHeadingText] = useState("Hello");
+  
   function handleClick(){
-    console.log("clicked!");
+    // console.log("clicked!");
+    setHeadingText("Submitted!");
+  }
+
+  function handleMouseOver(){
+    
+  }
+
+  function handleMouseOut(){
+    
   }
 
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button onClick={handleClick}>Submit</button>
+      <button style={{ backgroundColor: "white" }}
+        onClick={handleClick} 
+        onMouseOver={handleMouseOver} 
+        onMouseOut={handleMouseOut} >
+          Submit
+      </button>
     </div>
   );
 }
