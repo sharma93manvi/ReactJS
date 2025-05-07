00cb6441b9b14797742bfa77a959871a4fa9ec5b
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-05-07 11:44:47 -0700

    Controlled Components: use name as single source of truth

--- 311-react-forms/src/components/App.jsx
+++ 311-react-forms/src/components/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
 
+  const [name, setName] = useState("");
+
   function handleChange() {
-    console.log("changed");
+    // console.log(event.target.placeholder);
+    // console.log(event.target.type);
+    console.log(event.target.value);
+    setName(event.target.value);
   }
 
   return (
     <div className="container">
-      <h1>Hello </h1>
+      <h1>Hello {name}</h1>
       <input 
         onChange={handleChange}
         type="text" 
